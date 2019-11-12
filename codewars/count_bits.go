28aa85d3b8102002a420a93111b881af2d53aaa9
keep_on_rail <keep_on_rail@intersvyaz.net>
2019-11-12 12:53:04 +0500

    count_bits task done

--- codewars/count_bits.go
+++ codewars/count_bits.go
@@ -1,19 +1,26 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-//A function that takes an integer as input,
+// A function that takes an integer as input,
 // and returns the number of bits that are equal to one in the binary representation of that number.
 // You can guarantee that input is non-negative.
 //
-//Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case.
-func CountBits(shit uint) uint {
-	fmt.Println("lool")
-	return shit
+//Example: The binary representation of 1234 is 10011010010,
+// so the function should return 5 in this case.
+func CountBits(shit uint) int {
+	sum := 0
+	for i := 0; shit > 0; i++ {
+		res := shit / 2
+		rem := shit % 2
+		if rem != 0 {
+			sum += 1
+		}
+		shit = res
+	}
+	return sum
 }
 
 func main() {
-	CountBits(1234)
+	fmt.Println(CountBits(12312))
 }
--- codewars/count_bits_test.go
+++ codewars/count_bits_test.go
@@ -4,7 +4,8 @@ import "testing"
 
 func TestCountBits(t *testing.T) {
 	bin := CountBits(1234)
-	if bin != 10011010010 {
+	if bin != 5 {
 		t.Errorf("BIN was incorrect, got: %d, want: %d.", bin, 10011010010)
 	}
+
 }
