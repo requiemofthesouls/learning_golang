d0a51fe69826c38464699f0a761ad63bfef90288
requiemofthesouls <dimamid99@gmail.com>
2019-11-18 18:24:44 +0500

    https://www.codewars.com/kata/5506b230a11c0aeab3000c1f/train/go solved

--- codewars/evaporator/evaporator.go
+++ codewars/evaporator/evaporator.go
@@ -1,10 +1,35 @@
 package main
 
+import "fmt"
+
+//This program tests the life of an evaporator containing a gas.
+
+//We know the content of the evaporator (content in ml),
+// the percentage of foam or gas lost every day (evap_per_day)
+// and the threshold (threshold) in percentage beyond which the evaporator
+// is no longer useful. All numbers are strictly positive.
+
+//The program reports the nth day (as an integer) on which the evaporator will be out of use.
+
+//Note : Content is in fact not necessary in the body of the function "evaporator",
+// you can use it or not use it, as you wish. Some people might prefer to reason with content,
+// some other with percentages only.
+
+// It's up to you but you must keep it as a parameter because the tests have it as an argument.
 func Evaporator(content float64, evapPerDay int, threshold int) int {
 	// your code
-	return 0
+	var days int
+	thresholdMl := content / 100 * float64(threshold)
+
+	for content > thresholdMl {
+		amount := content / 100 * float64(evapPerDay)
+		content -= amount
+		days += 1
+	}
+
+	return days
 }
 
 func main() {
-
+	fmt.Println(Evaporator(10, 10, 10))
 }
--- codewars/evaporator/evaporator_test.go
+++ codewars/evaporator/evaporator_test.go
@@ -1,7 +1,13 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestEvaporator(t *testing.T) {
-
+	answ := Evaporator(10, 10, 10)
+	if answ != 22 {
+		fmt.Println("Wrong answer! expected 22, got", answ)
+	}
 }
